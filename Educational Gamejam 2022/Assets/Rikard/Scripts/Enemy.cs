ecaad3d0eb13bf9ad5e7666dfb66952fe5de57cb
RicheyDev <90191926+RicheyDev@users.noreply.github.com>
2022-02-24 15:38:41 +0100

    spawn

---
+++ Educational Gamejam 2022/Assets/Rikard/Scripts/Enemy.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    private float movementDuration = 2.0f;
+    private float waitBeforeMoving = 2.0f;
+    private bool hasArrived = false;
+
+    private void start()
+    {
+        movementDuration = Random.Range(5.0f, 10.0f);
+        waitBeforeMoving = Random.Range(0.5f, 3.0f);
+        
+    }
+    
+    private void Update()
+    {
+        if(!hasArrived)
+        {
+            hasArrived = true;
+            float randX = Random.Range(-3f, 8f);
+            float randY = Random.Range(-3f, 8f);
+            StartCoroutine(MoveToPoint(new Vector3(randX, randY, 0)));
+        }
+    }
+ 
+    private IEnumerator MoveToPoint(Vector3 targetPos)
+    {
+        float timer = 0.0f;
+        Vector3 startPos = transform.position;
+ 
+        while (timer < movementDuration)
+        {
+            timer += Time.deltaTime;
+            float t = timer / movementDuration;
+            t = t * t * t * (t * (6f * t - 15f) + 10f);
+            transform.position = Vector3.Lerp(startPos, targetPos, t);
+ 
+            yield return null;
+        }
+ 
+        yield return new WaitForSeconds(waitBeforeMoving);
+        hasArrived = false;
+    }
+}
---
+++ Educational Gamejam 2022/Assets/Rikard/Scripts/WaveSpawner.cs
@@ -0,0 +1,157 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveSpawner : MonoBehaviour
+{
+    public enum SpawnState {spawning, waiting, counting};
+    public bool ableToSpawn; 
+        
+    //Objects
+    public GameObject[] barsList;
+
+    [System.Serializable]
+    public class Wave
+    {
+        public string name;
+        public Transform enemy;
+        public int count;
+        public float rate;
+    }
+
+    public Wave[] waves;
+    private int nextWave = 0;
+
+    public Transform[] spawnPoints;
+
+    public float timeBetweenWaves = 5f;
+    public float waveCountDown;
+
+    public float timeBtwSpawns = 1f;
+
+    public SpawnState state = SpawnState.counting;
+
+    private void Start()
+    {
+        ableToSpawn = false;
+
+        waveCountDown = timeBetweenWaves;
+
+        if (spawnPoints.Length == 0)
+        {
+            Debug.Log("Error, No Spawn Points Available");
+        }
+    }
+
+    private void Update()
+    {
+        if (ableToSpawn)
+        {
+            if (state == SpawnState.waiting)
+            {
+                if (!EnemyIsAlive())
+                {
+                    WaveCompleted();
+                    return;
+                }
+                else
+                {
+                    return;
+                }
+            }
+
+            if (waveCountDown <= 0)
+            {
+                if (state != SpawnState.spawning)
+                {
+                    StartCoroutine(SpawnWave(waves[nextWave]));
+                }
+            }
+            else
+            {
+                waveCountDown -= Time.deltaTime;
+            }
+        }
+
+    }
+
+    void WaveCompleted()
+    {
+        state = SpawnState.counting;
+        waveCountDown = timeBetweenWaves;
+
+        if(nextWave + 1 > waves.Length - 1)
+        {
+            //All waves completed
+            foreach (GameObject bars in barsList)
+            {
+                bars.GetComponent<SpriteRenderer>().enabled = true;
+            }
+
+            ableToSpawn = false;
+            Destroy(transform.parent.gameObject);
+        }
+        else
+        {
+        nextWave++;
+        }
+
+    }
+
+
+    bool SpawnBosses()
+    {
+        timeBtwSpawns -= Time.deltaTime;
+
+        if (timeBtwSpawns <= 0f)
+        {
+            timeBtwSpawns = Random.Range(1, 20);
+
+            if (GameObject.FindGameObjectsWithTag("Enemy Spawned").Length <= 0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+
+    bool EnemyIsAlive()
+    {
+        timeBtwSpawns -= Time.deltaTime;
+
+        if (timeBtwSpawns <= 0f)
+        {
+        timeBtwSpawns = Random.Range(1, 20);
+
+            //Activate this if we want to kill them jellies
+            // if (GameObject.FindGameObjectsWithTag("Enemy Spawned").Length <= 0)
+            // {
+            //     return false;
+            // }
+    }
+        return true;
+    }
+
+    IEnumerator SpawnWave (Wave _wave)
+    {
+        state = SpawnState.spawning;
+
+        for (int i = 0; i < _wave.count; i++)
+        {
+            SpawnEnemy(_wave.enemy);
+            yield return new WaitForSeconds(_wave.rate);
+        }
+        state = SpawnState.waiting;
+
+        yield break;
+    }
+
+    void SpawnEnemy (Transform _enemy)
+    {
+        Transform _sp = spawnPoints[Random.Range(0, spawnPoints.Length)];
+        Instantiate(_enemy, _sp.position, _sp.rotation);
+    }
+
+}
