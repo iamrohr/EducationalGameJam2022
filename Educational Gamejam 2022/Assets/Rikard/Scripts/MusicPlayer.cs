7cb12e5c3f0d77b6d7f0a4190c1726065d2814a6
RicheyDev <90191926+RicheyDev@users.noreply.github.com>
2022-02-24 20:30:31 +0100

    Music player

---
+++ Educational Gamejam 2022/Assets/Rikard/Scripts/MusicPlayer.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour
+{
+    AudioSource m_AudioSource;
+    
+
+    void Start()
+    {
+        m_AudioSource.Play();
+        
+    }
+
+    void Update()
+    {
+      
+    }
+
+    //This plays the Audio clip when you press the Button
+    void PlayButton()
+    {
+     
+    }
+}
